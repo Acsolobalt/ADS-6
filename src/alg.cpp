682eeb31ced98d5ac68b1c036cf9217c76c36743
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-05-30 17:04:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,27 +7,27 @@
 
 BST<std::string> makeTree(const char* filename) {
   BST<std::string>tree;
-char changeSize = 'a' - 'A';
-std::string keyword = "";
-std::ifstream file(filename);
-if (!file) {
-std::cout << "File error!" << std::endl;
-return;
-}
-while (!file.eof()) {
-char ch = file.get();
-if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
-if (ch >= 'A' && ch <= 'Z') {
-ch += changeSize;
-}
-keyword += ch;
-} else {
-if (keyword != "") {
-tree.add(keyword);
-keyword = "";
-}
-}
-}
-file.close();
-return tree;
+    char changeSize = 'a' - 'A';
+    std::string keyword = "";
+    std::ifstream file(filename);
+    if (!file) {
+      std::cout << "File error!" << std::endl;
+      return;
+    }
+    while (!file.eof()) {
+      char ch = file.get();
+      if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+        if (ch >= 'A' && ch <= 'Z') {
+        ch += changeSize;
+      }
+      keyword += ch;
+      } else {
+        if (keyword != "") {
+          tree.add(keyword);
+          keyword = "";
+        }
+      }
+    }
+    file.close();
+    return tree;
 }
