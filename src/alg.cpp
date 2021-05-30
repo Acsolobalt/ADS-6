3f9dc263905ac6bacb5e59ec7e9c866fa594db7a
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-05-30 11:50:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,30 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  	BST<std::string> tree;
+	char changeSize = 'a' - 'A';
+	std::string keyword = "";
+	std::ifstream file(filename);
+	if (!file) {
+		std::cout << "File error!" << std::endl;
+		return;
+	}
+	while (!file.eof())
+	{
+		char ch = file.get();
+		if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+			if (ch >= 'A' && ch <= 'Z') {
+				ch += changeSize;
+			}
+			keyword += ch;
+		}
+		else {
+			if (keyword != "") {
+				tree.add(keyword);
+				keyword = "";
+			}
+		}
+	}
+	file.close();
+	return tree;
 }
