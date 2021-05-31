b8571b4d44a48cd628575119fe984a759d454caf
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-05-31 14:23:17 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -8,92 +8,58 @@ class BST {
 struct Node {
 T value;
 int count;
-Node * left;
-Node * right;
-unsigned char height;
-Node(T k) : value(k), left(nullptr), right(nullptr), height(1), count(1) {}
+Node* left;
+Node* right;
 };
 
  private:
-Node * root;
-int depthTree(Node * root) {
+Node* root;
+Node* addNode(Node*, T) {
 if (root == nullptr) {
-return 0;
+root = new Node;
+root->value = value;
+root->count = 1;
+root->left = root->right = nullptr;
+} else if (root->value > value) {
+root->left = addNode(root->left, value);
+} else if (root->value < value) {
+root->right = addNode(root->right, value);
 } else {
-int lh = depthTree(root->left);
-int rh = depthTree(root->right);
-return (lh > rh ? lh : rh) + 1;
+root->count++;
 }
+return root;
 }
-int searchNode(Node * root, T value) {
+int depthTree(Node*) {
 if (root == nullptr)
 return 0;
-else if (root->value == value)
-return root->count;
-else if (value < root->value)
+if (root->left == nullptr && root->right == nullptr)
+return 0;
+int depthL = depthTree(root->left);
+int depthR = depthTree(root->right);
+return ((depthL > depthR ? depthL : depthR) + 1);
+}
+int searchNode(Node*, T) {
+if (root == nullptr) {
+return 0;
+} else if (root->value > value) {
 return searchNode(root->left, value);
-else
+} else if (root->value < value) {
 return searchNode(root->right, value);
+} else {
+return root->count;
 }
-unsigned char height(Node * p) {
-return p ? p->height :0;
-}
-int bfactor(Node * p) {
-return height(p->right) - height(p->left);
-}
-void fixheight(Node * p) {
-unsigned char hl = height(p->left);
-unsigned char hr = height(p->right);
-p->height = (hl > hr ? hl : hr) + 1;
-}
-Node * rotateright(Node * p) {
-Node * q = p->left;
-p->left = q->right;
-q->right = p;
-fixheight(p);
-fixheight(q);
-return q;
-}
-Node * rotateleft(Node * q) {
-Node* p = q->right;
-q->right = p->left;
-p->left = q;
-fixheight(q);
-fixheight(p);
-return p;
-}
-Node * balance(Node * p) {
-fixheight(p);
-if (bfactor(p) == 2) {
-if (bfactor(p->right) < 0)
-p->right = rotateright(p->right);
-return rotateleft(p);
-}
-if (bfactor(p) == -2) {
-if (bfactor(p->left) > 0)
-p->left = rotateleft(p->left);
-return rotateright(p);
-}
-return p;
-}
-Node * insert(Node * p, T k) {
-if (!p) return new Node(k);
-if (k < p->value)
-p->left = insert(p->left, k);
-else
-p->right = insert(p->right, k);
-return balance(p);
 }
 
  public:
-BST() : root(nullptr) {}
-void add(T value) {
-root = insert(root, value);
+BST():root(nullptr) {}
+~BST();
+void add(T) {
+root = addNode(root, value);
 }
 int depth() {
 return depthTree(root);
 }
-int search(T value) {
+int search(T) {
 return searchNode(root, value);
 }
 };
